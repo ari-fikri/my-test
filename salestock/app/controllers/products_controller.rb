648a9aa9a9f677a983d2e5607bfe6caddc7ed64a
Ari Fikri <ari.fikri@lippox.com>
2016-06-19 10:02:58 +0700

    - Add Update and delete for product - Remove SQLite gem

--- salestock/app/controllers/products_controller.rb
+++ salestock/app/controllers/products_controller.rb
@@ -8,5 +8,27 @@ class ProductsController < ApplicationController
         @product = Product.find(params[:id])
         render json: @product
     end
-    
+
+    def update
+        @product = Product.find(params[:id])
+
+        if @product.update_attributes(:name => params[:name], :price => params[:price], :color => params[:color], :description => params[:description])
+            render :json => {:msg => "Data is updated", :status => 1}
+        else
+            render :json => {:errors => @product.errors, :status => 0, :msg => "Something is wrong"}
+        end
+
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+
+        if @product.destroy
+            render :json => {:msg => "Data is Deleted", :status => 1}
+        else
+            render :json => {:errors => @product.errors, :status => 0, :msg => "Something is wrong"}
+        end
+
+    end
+
 end
