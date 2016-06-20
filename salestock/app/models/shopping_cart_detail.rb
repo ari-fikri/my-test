2b6431f3e1b646915561b4cee912e37ae574a913
Ari Fikri <ari.fikri@lippox.com>
2016-06-20 14:53:55 +0700

    remove plural statement and change into singular

--- salestock/app/models/shopping_cart_detail.rb
+++ salestock/app/models/shopping_cart_detail.rb
@@ -3,5 +3,5 @@ class ShoppingCartDetail < ActiveRecord::Base
     belongs_to :shopping_cart
 
     validates :qty, :product, presence: true
-    accepts_nested_attributes_for :products
+    accepts_nested_attributes_for :product
 end
